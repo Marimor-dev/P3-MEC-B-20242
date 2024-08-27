7d922b23e25bbb82efc7af4fb56ad3db24fe6510
Marimor-dev <est.mariac.moralesm@unimilitar.edu.co>
2024-08-27 15:45:05 -0500

    Create pg3.java

---
+++ P1/pg3.java
@@ -0,0 +1,10 @@
+public class triangulo {
+class static  metodo(int){
+    scanner op = new scanner (system.in);
+    int operacion
+    double n1,n2;
+    double n3;
+    double result;
+    result= (n2+n1);
+};
+}
